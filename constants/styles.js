852013fa406ec80814616c2338e1887100c16c2c
Arthur Lee <ajlee@impactnetworking.com>
2022-12-13 13:49:25 -0600

    updating

---
+++ constants/styles.js
@@ -0,0 +1,13 @@
+export const Color = {
+    black: "#222222",
+    blackGreen: "#002E2A",
+    gray: "#DFDFDF",
+    red100: "#DC092C",
+    red200: "#89051B",
+    yellow100: "#F9E34F",
+    yeallow300: "#A28C11",
+    blue100: "#28ABFD",
+    blue200: "#176A9F",
+    green100: "#4FAE5D",
+    green200: "#337446",
+}
