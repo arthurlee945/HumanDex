2efc968a9d56c63741fed4339a76b920a1792e13
Arthur Lee <ajlee@impactnetworking.com>
2022-12-13 14:52:42 -0600

    update

---
+++ screens/AboutScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function AboutScreen() {
+    return <Text>AboutScreen</Text>;
+}
+
+export default AboutScreen;
---
+++ screens/DexScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function DexScreen() {
+    return <Text>DexScreen</Text>;
+}
+
+export default DexScreen;
